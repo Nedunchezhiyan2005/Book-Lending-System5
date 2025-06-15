77f85e2018c9a7f5a9ad39196081032c3e3f1d80
Nedunchezhiyan2005 <nedunchezhiyan532@gmail.com>
2025-06-16 00:55:28 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnvZM65Dtp0nuhvA-NTO9zDub29f6E8vU",
-  authDomain: "library-book-lending-system.firebaseapp.com",
-  projectId: "library-book-lending-system",
-  storageBucket: "library-book-lending-system.appspot.com",
-  messagingSenderId: "44095976833",
-  appId: "1:44095976833:web:ed52d1e19d9b68e2ad2480",
-  measurementId: "G-PYV1E5JL3X",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
